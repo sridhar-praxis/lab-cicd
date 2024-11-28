b9734af21458c03a58dc38e6f14d57bf670e0e6b
Vol Ume <sridhar@praxis.ac.in>
2024-11-28 21:57:09 +0530

    update third

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, CI/CD with FastAPI second update!"}
+    return {"message": "Hello, CI/CD with FastAPI third update!"}
